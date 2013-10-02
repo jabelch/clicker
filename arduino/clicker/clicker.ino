9f47feea8c613ca4e5b8154a8971d50da7d42d4e
Josh <jabelch@gmail.com>
2013-10-02 00:40:17 -0600

    Just to get things started
    Arduino file to begin serial communication with the computer. Does
    absolutely nothing right now.

---
+++ arduino/clicker/clicker.ino
@@ -0,0 +1,14 @@
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial){
+    ;//Wait for serial port to connect
+  }
+  
+  Serial.println("Authors:");
+  Serial.println("
+}
+
+void loop() {
+  
+}
